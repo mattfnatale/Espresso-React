9f6abf18676a77f08dc2e0160fb2462e30507a98
mattfnatale <139235725+mattfnatale@users.noreply.github.com>
2023-07-12 14:03:17 -0700

    Convert data to json

--- App.js
+++ App.js
@@ -5,78 +5,88 @@ const PAGE_TITLE = "Espresso";
 const PAGE_SUB_TITLE = "SPA Demo";
 
 const HEADER_DATA = [
-  "Name",
-  "Blend",
-  "Acidity",
-  "Flavor Profile"
+  {title: "Name", value: "name"},
+  {title: "Blend", value: "blend"},
+  {title: "Acidity", value: "acidity"},
+  {title: "Flavor Profile", value: "flavorProfile"}
 ];
 
 const TABLE_DATA = [
-  ["Ecstatic",          "Dark",     "Low Medium",   "Butterscotch, Dark Chocolate, Dried Citrus"],
-  ["Jacob's Wonderbar", "Dark",     "Low",          "Dark Chocolate, Dried Berry, Smoke"],
-  ["Tesora",            "Medium",   "Very Low",     "Cocoa, Caramel, Almond"],
-  ["Philtered Soul",    "Medium",   "Medium Low",   "Maple, Hazelnut, Caramel"],
-  ["Ambrosia",          "Light",    "Low",          "Milk Chocolate, Almond, Vanilla"],
-  ["Unplugged Decaf",   "Dark",     "Medium",       "Semi Sweet Chocolate, Dried Plum"],
+  {name: "Ecstatic",          blend: "Dark",    acidity: "Low Medium",  flavorProfile: "Butterscotch, Dark Chocolate, Dried Citrus"},
+  {name: "Jacob's Wonderbar", blend: "Dark",    acidity: "Low",         flavorProfile: "Dark Chocolate, Dried Berry, Smoke"},
+  {name: "Tesora",            blend: "Medium",  acidity: "Very Low",    flavorProfile: "Cocoa, Caramel, Almond"},
+  {name: "Philtered Soul",    blend: "Medium",  acidity: "Medium Low",  flavorProfile: "Maple, Hazelnut, Caramel"},
+  {name: "Ambrosia",          blend: "Light",   acidity: "Low",         flavorProfile: "Milk Chocolate, Almond, Vanilla"},
+  {name: "Unplugged Decaf",   blend: "Dark",    acidity: "Medium",      flavorProfile: "Semi Sweet Chocolate, Dried Plum"}
 ];
 
-function TableHeader() {
-  let renderedHeader = HEADER_DATA.map(
-    function(headerName) {
-      return (<th className="headerCell">{headerName}</th>);
+function DataTable({data}) {
+  let renderedBody = data.map(
+    function(row, index) {
+      return (<TableRow key={"row-" + index} row={row} index={index} />)
   });
-  return (<tr>{renderedHeader}</tr>);
-}
-
-function TableBody() {
-  let renderedBody = TABLE_DATA.map(
-    function(row) {
-      return TableRow(row);
-    }
-  );
-  return renderedBody;
-}
-
-function TableRow(row) {
-  let renderedRow = row.map(
-    function(value) {
-      return (<td className="dataCell">{value}</td>);
-    }
-  );
-  return (<tr className="dataRow">{renderedRow}</tr>);
-}
-
-function DataTable() {
+  
   return (
     <table className="dataTable">
       <thead>
         <TableHeader />
       </thead>
       <tbody>
-        <TableBody />
+        {renderedBody}
       </tbody>
     </table>
+  );
+}
+
+function TableHeader() {
+  let renderedHeader = HEADER_DATA.map(
+    function(header, index) {
+      return (<th key={"header-" + header.value} className="headerCell">{header.title}</th>);
+  });
+  
+  return (<tr key="header">{renderedHeader}</tr>);
+}
+
+function TableRow({row}, {index}) {
+  let renderedRow = HEADER_DATA.map(
+    function(header, index) {
+      return (<td key={"row-" + index + "-" + header.value} className="dataCell">{row[header.value]}</td>);
+    }
+  );
+  
+  return (<tr key={"row-" + index} className="dataRow">{renderedRow}</tr>);
+}
+
+function Title() {
+  return (
+    <div className="title">
+      <h1 id="page-title">{PAGE_TITLE}</h1>
+      <h4 id="page-sub-title">{PAGE_SUB_TITLE}</h4>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div id="signature-text">
+        <h4>- Matt 🙂</h4>
+      </div>
+      <div id="source-text">
+        <h4><a target="_blank" rel='noreferrer' href="https://github.com/mattfnatale/Espresso-React">Source</a></h4>
+      </div>
+    </div>
   );
 }
 
 function App() {
   return (
     <div className="App">
-      <div className="title">
-        <h1 id="page-title">{PAGE_TITLE}</h1>
-        <h4 id="page-sub-title">{PAGE_SUB_TITLE}</h4>
-      </div>
+      <Title />
       <div className="content">
-        <DataTable />
+        <DataTable data={TABLE_DATA} />
       </div>
-      <div className="footer">
-        <div id="signature-text">
-          <h4>- Matt 🙂</h4>
-        </div>
-        <div id="source-text">
-          <h4><a target="_blank" href="https://github.com/mattfnatale/Espresso-React">Source</a></h4>
-        </div>
-      </div>
+      <Footer />
     </div>
   );
 }
